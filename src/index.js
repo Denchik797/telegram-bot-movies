6155c1496d91936c83ba81f26063e4d769e69d1a
Denchik797 <vladimirmakarov2401@gmail.com>
2022-06-01 21:07:07 +0300

    Создаём экземпляр класса Телеграм бота

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ const TelegramBot = require('node-telegram-bot-api')
 const config = require('./config')
 const helper = require('./helpers')
 
+const bot = new TelegramBot(config.TOKEN, {
+    polling: true
+})
