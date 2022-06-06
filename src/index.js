50b1926d6d4202ea1ae34d27c82d9f7b16915296
Denchik797 <vladimirmakarov2401@gmail.com>
2022-06-06 12:23:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const bot = new TelegramBot(config.TOKEN, {
 bot.on('message', msg => {
     console.log('Working', msg.from.first_name, msg.from.last_name, msg.from.username, msg.from.language_code) // Вывод данных о пользователе, который ведёт общение с ботом 
 
-
     switch (msg.text) {
         case kb.home.favourite:
             break
