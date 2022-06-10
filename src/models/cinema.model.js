32d1070cccbb8a593f532203f007016ec1e26aa6
Denchik797 <vladimirmakarov2401@gmail.com>
2022-06-11 00:15:40 +0300

    Создание модели

--- src/models/cinema.model.js
+++ src/models/cinema.model.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
+const CinemaSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    uuid: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: Schema.Types.Mixed
+    },
+    films: {
+        type: [String],
+        default: []
+    }
+})
