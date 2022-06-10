0d59ebd51215ba7c65f5a67a04441532eb939791
Denchik797 <vladimirmakarov2401@gmail.com>
2022-06-10 16:24:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ bot.onText(/\/f(.+)/, (msg, [source, match]) => {
     const chatId = helper.getChatId(msg)
     // console.log(filmUuid) вывод в консоль id фильма 
     Film.findOne({uuid: filmUuid}).then(film => {
-        const caption = `Название: ${film.name}\nГод: ${film.year}\nСтрана: ${film.country}\nРейтинг: ${film.rate}\nКоличество проголосовавших: ${film.ratingVoteCount}\nДлинна: ${film.length}\nОписание: ${film.description}`
+        const caption = `Название: ${film.name}\nГод: ${film.year}\nСтрана: ${film.country}\nРейтинг: ${film.rate}\nКоличество проголосовавших: ${film.ratingVoteCount}\nДлинна: ${film.length}`
         // console.log(film) вывод в консоль данных о фильме
         bot.sendPhoto(msg.chat.id, film.picture, {
             caption: caption
